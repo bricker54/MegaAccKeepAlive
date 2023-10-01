a317b6f542c862614078a3d6fa3d6d7596ffa830
bricker54 <142190062+bricker54@users.noreply.github.com>
2023-10-01 20:27:23 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 import requests
 from mega import Mega
-input("Warning! This will take an stupidly long time to finish. Press Enter to continue...")
-GigabyteTotalVal = 0.00
-try:
-    response = requests.get('https://meganzindex.000webhostapp.com/MegaAccess/MegaAccessData.data', auth=('TheVaultUsr', 'R0Y04>2~3[Pu!ch;;@yGjY%HVa>-vO[+0Ryq|#<bLi+VH*lhKl'))
-    response.raise_for_status()  # Raise an exception if the request was not successful
+import time
+while True:
+    GigabyteTotalVal = 0.00
+    try:
+        response = requests.get('https://meganzindex.000webhostapp.com/MegaAccess/MegaAccessData.data', auth=('TheVaultUsr', 'R0Y04>2~3[Pu!ch;;@yGjY%HVa>-vO[+0Ryq|#<bLi+VH*lhKl'))
+        response.raise_for_status()  # Raise an exception if the request was not successful
 
-    # Split the content of the webpage into lines
-    lines = response.text.split('\n') 
+        # Split the content of the webpage into lines
+        lines = response.text.split('\n') 
 
-    # Iterate through the lines and print each one
-    for i in range(len(i)):
-        mega = Mega()
-        CurrentRequest = lines[i].strip()
-        m = mega.login(CurrentRequest, "MultiupSucksAss54@#")
-        quota = m.get_quota()
-        MBVal = float(quota)
-        MBVal = MBVal / 1000
-        GigabyteTotalVal = GigabyteTotalVal + MBVal
+        # Iterate through the lines and print each one
+
+        for i in tqdm (range (len(lines)-1), desc="Requesting Mega.Nz for the quota data for "+str(len(lines)-1)+" accounts."):
+            mega = Mega()
+            CurrentRequest = lines[i].strip()
+            m = mega.login(CurrentRequest, "MultiupSucksAss54@#")
+            quota = m.get_quota()
+            MBVal = float(quota)
+            MBVal = MBVal / 1000
+            GigabyteTotalVal = GigabyteTotalVal + MBVal
+            pass
     
-    print("Total Space: "+str(GigabyteTotalVal)+" GB")
+        print("Total Space: "+str(GigabyteTotalVal)+" GB")
 
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
+        
+    time.sleep(30 * 24 * 60 * 60)
